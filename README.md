68f10335e81193552b68f075c08c35d5f40b2e64
Marshall Bowers <elliott.codes@gmail.com>
2019-02-09 16:21:17 -0500

    Add trailing slashes for docs.rs links

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Peacock
 
 [![Crates.io](https://img.shields.io/crates/v/peacock.svg)](https://crates.io/crates/peacock)
-[![Docs.rs](https://docs.rs/peacock/badge.svg)](https://docs.rs/peacock)
+[![Docs.rs](https://docs.rs/peacock/badge.svg)](https://docs.rs/peacock/)
 [![Crates.io](https://img.shields.io/crates/l/peacock.svg)](https://github.com/maxdeviant/peacock/blob/master/LICENSE)
 
 Peacock is a game engine for making beautiful games.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Peacock is a game engine for making beautiful games.
 //!
 //! [![Crates.io](https://img.shields.io/crates/v/peacock.svg)](https://crates.io/crates/peacock)
-//! [![Docs.rs](https://docs.rs/peacock/badge.svg)](https://docs.rs/peacock)
+//! [![Docs.rs](https://docs.rs/peacock/badge.svg)](https://docs.rs/peacock/)
 //! [![Crates.io](https://img.shields.io/crates/l/peacock.svg)](https://github.com/maxdeviant/peacock/blob/master/LICENSE)
 //!
 //! ## Installation
