3b49bf1e70871491011b022da32057c40851ff14
Marshall Bowers <elliott.codes@gmail.com>
2019-02-09 22:55:56 -0500

    Remove hard-coded values from sprite batching

--- src/graphics/sprite_batch.rs
+++ src/graphics/sprite_batch.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 use sfml::graphics::{
     Drawable, PrimitiveType, RenderStates, RenderTarget, Sprite, Texture, Transformable,
     VertexArray,
@@ -36,19 +34,20 @@ impl<'t> SpriteBatch<'t> {
             self.vertices.resize(self.vertices.vertex_count() + 4);
         }
 
-        self.vertices[index + 0].position = sprite.position();
-        self.vertices[index + 1].position = sprite.position().add(Vector2f::new(8.0, 0.0));
-        self.vertices[index + 2].position = sprite.position().add(Vector2f::new(8.0, 8.0));
-        self.vertices[index + 3].position = sprite.position().add(Vector2f::new(0.0, 8.0));
-
         let texture_rect = sprite.texture_rect();
-        let position = Vector2f::new(texture_rect.left as f32, texture_rect.top as f32);
         let size = Vector2f::new(texture_rect.width as f32, texture_rect.height as f32);
 
+        self.vertices[index + 0].position = sprite.position();
+        self.vertices[index + 1].position = sprite.position() + Vector2f::new(size.x, 0.0);
+        self.vertices[index + 2].position = sprite.position() + size;
+        self.vertices[index + 3].position = sprite.position() + Vector2f::new(0.0, size.y);
+
+        let position = Vector2f::new(texture_rect.left as f32, texture_rect.top as f32);
+
         self.vertices[index + 0].tex_coords = position;
-        self.vertices[index + 1].tex_coords = position.add(Vector2f::new(size.x, 0.0));
-        self.vertices[index + 2].tex_coords = position.add(size);
-        self.vertices[index + 3].tex_coords = position.add(Vector2f::new(0.0, size.y));
+        self.vertices[index + 1].tex_coords = position + Vector2f::new(size.x, 0.0);
+        self.vertices[index + 2].tex_coords = position + size;
+        self.vertices[index + 3].tex_coords = position + Vector2f::new(0.0, size.y);
 
         self.vertices[index + 0].color = sprite.color();
         self.vertices[index + 1].color = sprite.color();
