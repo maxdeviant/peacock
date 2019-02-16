8761e5ffe7c1dcc709b0887f55517d6035b2b43f
Marshall Bowers <elliott.codes@gmail.com>
2019-02-16 11:57:17 -0500

    Add example for showcasing colors

---
+++ examples/colors.rs
@@ -0,0 +1,210 @@
+use peacock::graphics::{self, Color, DrawSpriteParams, Sprite, Texture};
+use peacock::{Context, ContextBuilder, Result, State};
+
+struct GameState {
+    textures: Vec<Texture>,
+}
+
+impl GameState {
+    fn new() -> Result<Self> {
+        let all_colors = vec![
+            // Pinks
+            Color::PINK,
+            Color::LIGHT_PINK,
+            Color::HOT_PINK,
+            Color::DEEP_PINK,
+            Color::PALE_VIOLET_RED,
+            Color::MEDIUM_VIOLET_RED,
+            // Reds
+            Color::LIGHT_SALMON,
+            Color::SALMON,
+            Color::DARK_SALMON,
+            Color::LIGHT_CORAL,
+            Color::INDIAN_RED,
+            Color::CRIMSON,
+            Color::FIREBRICK,
+            Color::DARK_RED,
+            Color::RED,
+            // Oranges
+            Color::ORANGE_RED,
+            Color::TOMATO,
+            Color::CORAL,
+            Color::DARK_ORANGE,
+            Color::ORANGE,
+            // Yellows
+            Color::YELLOW,
+            Color::LIGHT_YELLOW,
+            Color::LEMON_CHIFFON,
+            Color::LIGHT_GOLDENROD_YELLOW,
+            Color::PAPAYA_WHIP,
+            Color::MOCCASIN,
+            Color::PEACH_PUFF,
+            Color::PALE_GOLDENROD,
+            Color::KHAKI,
+            Color::DARK_KHAKI,
+            Color::GOLD,
+            // Browns
+            Color::CORNSILK,
+            Color::BLANCHED_ALMOND,
+            Color::BISQUE,
+            Color::NAVAJO_WHITE,
+            Color::WHEAT,
+            Color::BURLYWOOD,
+            Color::TAN,
+            Color::ROSY_BROWN,
+            Color::SANDY_BROWN,
+            Color::GOLDENROD,
+            Color::DARK_GOLDENROD,
+            Color::PERU,
+            Color::CHOCOLATE,
+            Color::SADDLE_BROWN,
+            Color::SIENNA,
+            Color::BROWN,
+            Color::MAROON,
+            // Greens
+            Color::DARK_OLIVE_GREEN,
+            Color::OLIVE,
+            Color::OLIVE_DRAB,
+            Color::YELLOW_GREEN,
+            Color::LIME_GREEN,
+            Color::LIME,
+            Color::LAWN_GREEN,
+            Color::CHARTREUSE,
+            Color::GREEN_YELLOW,
+            Color::SPRING_GREEN,
+            Color::MEDIUM_SPRING_GREEN,
+            Color::LIGHT_GREEN,
+            Color::PALE_GREEN,
+            Color::DARK_SEA_GREEN,
+            Color::MEDIUM_AQUAMARINE,
+            Color::MEDIUM_SEA_GREEN,
+            Color::SEA_GREEN,
+            Color::FOREST_GREEN,
+            Color::GREEN,
+            Color::DARK_GREEN,
+            // Cyans
+            Color::AQUA,
+            Color::CYAN,
+            Color::LIGHT_CYAN,
+            Color::PALE_TURQUOISE,
+            Color::AQUAMARINE,
+            Color::TURQUOISE,
+            Color::MEDIUM_TURQUOISE,
+            Color::DARK_TURQUOISE,
+            Color::LIGHT_SEA_GREEN,
+            Color::CADET_BLUE,
+            Color::DARK_CYAN,
+            Color::TEAL,
+            // Blues
+            Color::LIGHT_STEEL_BLUE,
+            Color::POWDER_BLUE,
+            Color::LIGHT_BLUE,
+            Color::SKY_BLUE,
+            Color::LIGHT_SKY_BLUE,
+            Color::DEEP_SKY_BLUE,
+            Color::DODGER_BLUE,
+            Color::CORNFLOWER_BLUE,
+            Color::STEEL_BLUE,
+            Color::ROYAL_BLUE,
+            Color::BLUE,
+            Color::MEDIUM_BLUE,
+            Color::DARK_BLUE,
+            Color::NAVY,
+            Color::MIDNIGHT_BLUE,
+            // Purples, violets, and magentas
+            Color::LAVENDER,
+            Color::THISTLE,
+            Color::PLUM,
+            Color::VIOLET,
+            Color::ORCHID,
+            Color::FUCHSIA,
+            Color::MAGENTA,
+            Color::MEDIUM_ORCHID,
+            Color::MEDIUM_PURPLE,
+            Color::BLUE_VIOLET,
+            Color::DARK_VIOLET,
+            Color::DARK_ORCHID,
+            Color::DARK_MAGENTA,
+            Color::PURPLE,
+            Color::INDIGO,
+            Color::DARK_SLATE_BLUE,
+            Color::SLATE_BLUE,
+            Color::MEDIUM_SLATE_BLUE,
+            // Whites
+            Color::WHITE,
+            Color::SNOW,
+            Color::HONEYDEW,
+            Color::MINT_CREAM,
+            Color::AZURE,
+            Color::ALICE_BLUE,
+            Color::GHOST_WHITE,
+            Color::WHITE_SMOKE,
+            Color::SEASHELL,
+            Color::BEIGE,
+            Color::OLD_LACE,
+            Color::FLORAL_WHITE,
+            Color::IVORY,
+            Color::ANTIQUE_WHITE,
+            Color::LINEN,
+            Color::LAVENDER_BLUSH,
+            Color::MISTY_ROSE,
+            // Grays and blacks
+            Color::GAINSBORO,
+            Color::LIGHT_GRAY,
+            Color::SILVER,
+            Color::DARK_GRAY,
+            Color::GRAY,
+            Color::DIM_GRAY,
+            Color::LIGHT_SLATE_GRAY,
+            Color::SLATE_GRAY,
+            Color::DARK_SLATE_GRAY,
+            Color::BLACK,
+        ];
+
+        let mut textures = Vec::with_capacity(all_colors.len());
+        for color in all_colors {
+            textures.push(Texture::from_color((32, 32).into(), color)?);
+        }
+
+        Ok(Self { textures })
+    }
+}
+
+impl State for GameState {
+    fn update(&mut self, _ctx: &mut Context) -> Result<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context, _dt: f64) -> Result<()> {
+        graphics::clear(ctx, Color::BLACK);
+
+        let (width, height) = (12, 12);
+
+        for x in 0..width {
+            for y in 0..height {
+                let index = x + (y * width);
+                if index > self.textures.len() - 1 {
+                    break;
+                }
+                let texture = &self.textures[index];
+
+                let sprite = Sprite::with_texture(&texture.texture);
+                graphics::draw_sprite(
+                    ctx,
+                    &sprite,
+                    DrawSpriteParams {
+                        position: (x as f32 * 32.0, y as f32 * 32.0).into(),
+                    },
+                )
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let mut context = ContextBuilder::new("Colors", 384, 384).build()?;
+    let mut game_state = GameState::new()?;
+    context.run(&mut game_state)
+}
--- src/graphics/texture.rs
+++ src/graphics/texture.rs
@@ -1,18 +1,27 @@
-use sfml::graphics::Texture as SfTexture;
+use sfml::graphics::{Image as SfImage, Texture as SfTexture};
 
 use crate::error::Error;
-use crate::Result;
+use crate::graphics::Color;
+use crate::{Result, Vector2u};
 
 /// A texture.
 #[derive(Debug)]
 pub struct Texture {
-    pub(crate) texture: SfTexture,
+    // TODO: This shouldn't be public.
+    pub texture: SfTexture,
 }
 
 impl Texture {
     /// Creates a new [`Texture`] from a file.
     pub fn from_file(filename: &str) -> Result<Self> {
         let texture = SfTexture::from_file(filename).ok_or(Error)?;
+        Ok(Self { texture })
+    }
+
+    /// Creates a new [`Texture`] and fills it with the specified [`Color`].
+    pub fn from_color(size: Vector2u, color: Color) -> Result<Self> {
+        let image = SfImage::from_color(size.x, size.y, &color.into()).ok_or(Error)?;
+        let texture = SfTexture::from_image(&image).ok_or(Error)?;
         Ok(Self { texture })
     }
 }
