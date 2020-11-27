b4fd9543272f285b4d9b13c9b6b5c4ae07f3c7c8
Marshall Bowers <elliott.codes@gmail.com>
2020-11-27 17:51:06 -0500

    Expose more ECS operations

--- src/ecs.rs
+++ src/ecs.rs
@@ -18,6 +18,22 @@ pub fn create_entity(ctx: &mut Context) -> EntityBuilder {
     EntityBuilder::new(&mut ctx.world)
 }
 
+pub fn entities(ctx: &Context) -> Vec<Entity> {
+    ctx.world.entities.clone()
+}
+
+pub fn has_component<T: Component>(ctx: &mut Context, entity: Entity) -> bool {
+    ctx.world.has_component::<T>(entity)
+}
+
+pub fn get_component<T: Component>(ctx: &Context, entity: Entity) -> Option<&T> {
+    ctx.world.get_component(entity)
+}
+
+pub fn get_component_mut<T: Component>(ctx: &mut Context, entity: Entity) -> Option<&mut T> {
+    ctx.world.get_component_mut(entity)
+}
+
 pub fn delete_entity(ctx: &mut Context, entity: Entity) {
     unimplemented!()
 }
