9676af98be975976618fbd8390009fd3d0da562b
Marshall Bowers <elliott.codes@gmail.com>
2020-11-27 23:03:25 -0500

    Don't recreate text textures on each draw

--- examples/text.rs
+++ examples/text.rs
@@ -3,14 +3,20 @@ use peacock::Result;
 use peacock::{Context, ContextBuilder, State, Vector2f};
 
 struct GameState {
-    font: Font,
+    greeting_message: Text,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> Result<Self> {
         let font = Font::from_file(ctx, "examples/res/Roboto-Regular.ttf", 24)?;
 
-        Ok(Self { font })
+        let greeting_message = Text::new(
+            ctx,
+            "Hello, world!\n\nI hope you enjoy using Peacock!",
+            &font,
+        )?;
+
+        Ok(Self { greeting_message })
     }
 }
 
@@ -20,13 +26,9 @@ impl State for GameState {
     }
 
     fn draw(&mut self, ctx: &mut Context, _dt: f64) -> Result<()> {
-        let text = Text::new(
-            "Hello, world!\n\nI hope you enjoy using Peacock!",
-            &self.font,
-        );
         graphics::draw(
             ctx,
-            &text,
+            &self.greeting_message,
             &DrawTextParams {
                 position: Vector2f::new(10.0, 10.0),
             },
--- src/graphics/text.rs
+++ src/graphics/text.rs
@@ -1,19 +1,38 @@
 use sdl2::rect::Rect as SdlRect;
 
 use crate::error::{AnyhowContext, Sdl2Error};
-use crate::graphics::{Color, Drawable, Font};
+use crate::graphics::{AssetRef, Color, Drawable, Font};
 use crate::vector2::Vector2f;
 use crate::{Context, Result};
 
 #[derive(Debug)]
-pub struct Text<'a> {
-    pub(crate) string: &'a str,
-    pub(crate) font: &'a Font,
+pub struct Text {
+    pub(crate) string: String,
+    pub(crate) texture: AssetRef,
 }
 
-impl<'a> Text<'a> {
-    pub fn new(string: &'a str, font: &'a Font) -> Self {
-        Self { string, font }
+impl Text {
+    pub fn new<S: Into<String>>(ctx: &mut Context, string: S, font: &Font) -> Result<Self> {
+        let texture_creator = ctx.canvas.texture_creator();
+
+        let string = string.into();
+        let surface = font
+            .font
+            .render(&string)
+            .blended(Color::WHITE)
+            .map_err(Sdl2Error::FontError)?;
+        let texture = texture_creator.create_texture_from_surface(&surface)?;
+
+        let texture_ref = AssetRef(ctx.graphics.counter);
+
+        ctx.graphics.counter += 1;
+
+        ctx.graphics.textures.insert(texture_ref, texture);
+
+        Ok(Self {
+            string,
+            texture: texture_ref,
+        })
     }
 }
 
@@ -24,20 +43,13 @@ pub struct DrawTextParams {
     pub position: Vector2f,
 }
 
-impl<'a> Drawable for Text<'a> {
+impl Drawable for Text {
     type Params = DrawTextParams;
 
     fn draw(&self, ctx: &mut Context, params: &DrawTextParams) -> Result<()> {
-        let texture_creator = ctx.canvas.texture_creator();
-
-        let surface = self
-            .font
-            .font
-            .render(self.string)
-            .blended(Color::WHITE)
-            .map_err(Sdl2Error::FontError)?;
-        let texture = texture_creator.create_texture_from_surface(&surface)?;
+        let texture = ctx.graphics.textures.get(&self.texture).unwrap();
         let texture_query = texture.query();
+
         ctx.canvas
             .copy(
                 &texture,
@@ -51,10 +63,6 @@ impl<'a> Drawable for Text<'a> {
             )
             .map_err(Sdl2Error::ErrorMessage)
             .context("Failed to copy texture to canvas")?;
-
-        unsafe {
-            texture.destroy();
-        }
 
         Ok(())
     }
